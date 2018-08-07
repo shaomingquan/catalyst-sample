689b0a782aa2623a3d8b017603a5588761475b07
shaomingquan <shaomingquan@bytedance.com>
2018-08-07 16:15:28 +0800

    add port

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"io/ioutil"
 	"log"
+	"os"
 
 	core "github.com/shaomingquan/webcore/core"
 )
@@ -13,12 +15,20 @@ var prepare = make(chan int)
 var loaded = make(chan int)
 
 func init() {
+	port := 0
+	flag.IntVar(&port, "port", 0, "app start port")
+	flag.CommandLine.Parse(os.Args[1:])
+
 	confbyted, err := ioutil.ReadFile("./appconf.json")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	conf := core.Conf{}
 	json.Unmarshal(confbyted, &conf)
+	if port != 0 {
+		conf.Port = port
+	}
+
 	app = core.App{
 		Config: &conf,
 	}
